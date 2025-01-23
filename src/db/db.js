bb8a52343348889d9ebb2c0c2b1e51d67c14586b
Carlos Figueroa <figuefive@gmail.com>
2025-01-23 15:24:46 -0500

    reajustar el repositorio

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA9TKrqyzrEdzTruLI-Ui3dz16nbBKJGBg",
-  authDomain: "coderhouse-react-d5b2a.firebaseapp.com",
-  projectId: "coderhouse-react-d5b2a",
-  storageBucket: "coderhouse-react-d5b2a.firebasestorage.app",
-  messagingSenderId: "772750019294",
-  appId: "1:772750019294:web:d8f427b2b67ce4aa3fd04f"
-};
-
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
